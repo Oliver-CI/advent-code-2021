0617ac458f80c6089ab56227ab6e7e8a75c1dba8
Oliver-CI <>
2021-12-12 13:30:12 +0100

    Add setup

---
+++ src/main/java/day14/Puzzle.java
@@ -0,0 +1,16 @@
+package day14;
+
+import util.FileUtil;
+
+import java.io.InputStream;
+
+public class Puzzle {
+    public static void main(String[] args) {
+        final String fileName = "day10/example.txt";
+//        final String fileName = "day10/input.txt";
+        final InputStream inputStream = FileUtil.readFile(fileName);
+
+
+        System.out.println("Result: ");
+    }
+}

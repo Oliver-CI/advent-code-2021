8d2025387234a207bb59ffeb6fa859d8f093ca5b
Oliver-CI <>
2021-12-22 14:49:51 +0100

    Part 2 Puzzle 22

--- src/main/java/day22/Reactor.java
+++ src/main/java/day22/Reactor.java
@@ -2,13 +2,19 @@ package day22;
 
 import util.IterativeSolver;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Reactor implements IterativeSolver {
-    private static final int MAX_DIMENSION = 50;
-    private static final int MIN_DIMENSION = -50;
     public static final String REGEX_RANGE = "\\..";
     public static final String REGEX_VAR_ASSIGNMENT = "=";
 
-    private final int[][][] grid = new int[2 * MAX_DIMENSION + 1][2 * MAX_DIMENSION + 1][2 * MAX_DIMENSION + 1];
+    private boolean[][][] grid;
+    private final List<Cube> cubes = new ArrayList<>();
+    private final List<Long> xes = new ArrayList<>();
+    private final List<Long> yes = new ArrayList<>();
+    private final List<Long> zes = new ArrayList<>();
 
     @Override
     public void iterate(String line) {
@@ -17,35 +23,67 @@ public class Reactor implements IterativeSolver {
         var coor = split[1];
         var coors = coor.split(",");
         var xss = coors[0].split(REGEX_VAR_ASSIGNMENT)[1].split(REGEX_RANGE);
-        var x0 = Math.max((Integer.parseInt(xss[0])), MIN_DIMENSION);
-        var x1 = Math.min((Integer.parseInt(xss[1])), MAX_DIMENSION);
+        var x0 = Long.parseLong(xss[0]);
+        var x1 = Long.parseLong(xss[1]);
         var yss = coors[1].split(REGEX_VAR_ASSIGNMENT)[1].split(REGEX_RANGE);
-        var y0 = Math.max((Integer.parseInt(yss[0])), MIN_DIMENSION);
-        var y1 = Math.min(Integer.parseInt(yss[1]), MAX_DIMENSION);
+        var y0 = Long.parseLong(yss[0]);
+        var y1 = Long.parseLong(yss[1]);
         var zss = coors[2].split(REGEX_VAR_ASSIGNMENT)[1].split(REGEX_RANGE);
-        var z0 = Math.max((Integer.parseInt(zss[0])), MIN_DIMENSION);
-        var z1 = Math.min(Integer.parseInt(zss[1]), MAX_DIMENSION);
-        final int enableCube = "on".equals(on) ? 1 : 0;
-//        System.out.printf("x: %d, %d, y: %d, %d, z: %d, %d\n", x0, x1, y0, y1, z0, z1);
+        var z0 = Long.parseLong(zss[0]);
+        var z1 = Long.parseLong(zss[1]);
 
-        for (int x = x0; x <= x1; x++) {
-            for (int y = y0; y <= y1; y++) {
-                for (int z = z0; z <= z1; z++) {
-                    grid[x + MAX_DIMENSION][y + MAX_DIMENSION][z + MAX_DIMENSION] = enableCube;
-                }
-            }
-        }
+        cubes.add(new Cube("on".equals(on), x0, y0, z0, x1, y1, z1));
+        xes.add(x0);
+        xes.add(x1);
+        yes.add(y0);
+        yes.add(y1);
+        zes.add(z0);
+        zes.add(z1);
     }
 
     public long getResult() {
+        Collections.sort(xes);
+        Collections.sort(yes);
+        Collections.sort(zes);
+        final int dimension = xes.size();
+
+        grid = new boolean[dimension][dimension][dimension];
+
+        for (Cube cube : cubes) {
+            //this map the actual values to a new grid to handle the big load
+            var x0 = xes.indexOf(cube.x0);
+            var x1 = xes.indexOf(cube.x1);
+            var y0 = yes.indexOf(cube.y0);
+            var y1 = yes.indexOf(cube.y1);
+            var z0 = zes.indexOf(cube.z0);
+            var z1 = zes.indexOf(cube.z1);
+
+            for (int x = x0; x < x1; x++) {
+                for (int y = y0; y < y1; y++) {
+                    for (int z = z0; z < z1; z++) {
+                        grid[x][y][z] = cube.enable;
+                    }
+                }
+            }
+        }
+
         long total = 0;
-        for (int x = MIN_DIMENSION; x <= MAX_DIMENSION; x++) {
-            for (int y = MIN_DIMENSION; y <= MAX_DIMENSION; y++) {
-                for (int z = MIN_DIMENSION; z <= MAX_DIMENSION; z++) {
-                    total += grid[x + MAX_DIMENSION][y + MAX_DIMENSION][z + MAX_DIMENSION];
+        for (int x = 0; x < dimension - 1; x++) {
+            for (int y = 0; y < dimension - 1; y++) {
+                for (int z = 0; z < dimension - 1; z++) {
+                    if (grid[x][y][z]) {
+                        final long xValue = xes.get(x + 1) - xes.get(x);
+                        final long yValue = yes.get(y + 1) - yes.get(y);
+                        final long zValue = zes.get(z + 1) - zes.get(z);
+                        total += xValue * yValue * zValue;
+                    }
                 }
             }
         }
         return total;
+    }
+
+    record Cube(boolean enable, long x0, long y0, long z0, long x1, long y1, long z1) {
+
     }
 }
